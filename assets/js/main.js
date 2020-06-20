04f57877bfd7428dde3e3def47c3b69f38015b2b
Caio Augusto <62035389+CaioAugustoo@users.noreply.github.com>
2020-06-20 18:48:28 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,24 +15,24 @@ function resultadoFormulario(event) {
   // Com os valores dos inputs obtidos, calculamos para obter o resultado do IMC ( Peso dividido por altura² )
   const imcTotal = (inputPeso / (inputAltura * inputAltura)).toFixed(2);
   
-  // Se o IMC do usuário for menor que 18,5 execute essa linha:
-   if (imcTotal >= 0 && imcTotal <= 18.5){
+  // Se o IMC do usuário for menor que 18,49 execute essa linha:
+   if (imcTotal >= 0.00 && imcTotal <= 18.49){
     resultado.innerHTML = `<p> Seu IMC é de: <strong> ${imcTotal} </strong> - Isso significa que você é muito magro. &#x1F626;  </p>`
   }
-  // Se o IMC do usuário estiver entre 18,6 e 24,9 execute essa linha:
-   else if (imcTotal >= 18.6 && imcTotal <= 24.9) {
+  // Se o IMC do usuário estiver entre 18,5 e 24,9 execute essa linha:
+   else if (imcTotal >= 18.50 && imcTotal <= 24.99) {
     resultado.innerHTML = `<p> Seu IMC é de: <strong> ${imcTotal} </strong> - Seu IMC é considerado "Normal". &#x1F604;  </p>`
   }
-  // Se o IMC do usuário estiver entre 25,0 e 29,9 execute essa linha:
-   else if (imcTotal >= 25.0 && imcTotal <= 29.9) {
+  // Se o IMC do usuário estiver entre 25,00 e 29,9 execute essa linha:
+   else if (imcTotal >= 25.00 && imcTotal <= 29.99) {
     resultado.innerHTML = `<p> Seu IMC é de: <strong> ${imcTotal} </strong> - Isso significa que sua classificação é "Sobrepeso". &#x1F615;  </p>`
   }
-  // Se o IMC do usuário estiver entre 30,0 e 39,9 execute essa linha:
-   else if (imcTotal >= 30.0 && imcTotal <= 39.9) {
+  // Se o IMC do usuário estiver entre 30,00 e 39,9 execute essa linha:
+   else if (imcTotal >= 30.00 && imcTotal <= 39.99) {
     resultado.innerHTML = `<p> Seu IMC é de: <strong> ${imcTotal} </strong> - Você é considerado "Obeso". &#128559;  </p>`
   } 
-  // Se o IMC do usuário for maior que 40,0 execute essa linha:
-   else if (imcTotal >= 40.0) {
+  // Se o IMC do usuário for maior que 40,00 execute essa linha:
+   else if (imcTotal >= 40.00) {
     resultado.innerHTML = `<p> Seu IMC é de: <strong> ${imcTotal} </strong> - Sua classificação é "Obesidade grave", 
     em um terceiro grau de obesidade. Você precisa de ajuda? &#x1F630;  </p>`
   }
